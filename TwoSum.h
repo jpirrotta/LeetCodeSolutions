314d59787903eeb66aa726200af862d6376051a2
Jesse Pirrotta <jessepirrotta@gmail.com>
2025-03-19 00:35:44 -0400

    Add initial implementation of Two Sum problem with CMake support

---
+++ TwoSum.h
@@ -0,0 +1,15 @@
+//
+// Created by jesse on 2025-03-19.
+//
+
+#ifndef TWOSUM_H
+#define TWOSUM_H
+
+#include <vector>
+
+class TwoSum {
+public:
+    static std::vector<int> twoSum(std::vector<int>& nums, int target);
+};
+
+#endif //TWOSUM_H
