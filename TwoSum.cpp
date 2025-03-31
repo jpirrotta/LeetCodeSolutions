a29c03d6085d32640598ba73034674bded2f726a
Jesse Pirrotta <jessepirrotta@gmail.com>
2025-03-30 20:26:21 -0400

    Whatever

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -5,15 +5,19 @@
 #include "TwoSum.h"
 #include <unordered_map>
 
-std::vector<int> TwoSum::twoSum(std::vector<int> &nums, int target) {
+std::vector<int> TwoSum::twoSum(std::vector<int> &nums, int target)
+{
     std::unordered_map<int, int> hashTbl;
     const int size = nums.size();
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         hashTbl[nums[i]] = i;
     }
 
-    for (int i = 0; i < size; i++) {
-        if (hashTbl.contains(target - nums[i]) && hashTbl[target - nums[i]] != i) {
+    for (int i = 0; i < size; i++)
+    {
+        if (hashTbl.contains(target - nums[i]) && hashTbl[target - nums[i]] != i)
+        {
             return {i, hashTbl[target - nums[i]]};
         }
     }
--- main.cpp
+++ main.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
-        #include <vector>
-        #include "TwoSum.h"
+#include <vector>
+#include "TwoSum.h"
 
-        void displayHeader(const std::string& date, const std::string& problemName) {
-            std::cout << "=============================" << std::endl;
-            std::cout << "LeetCode Problem: " << problemName << std::endl;
-            std::cout << "Date: " << date << std::endl;
-            std::cout << "=============================" << std::endl;
-        }
+void displayHeader(const std::string &date, const std::string &problemName)
+{
+    std::cout << "=============================" << std::endl;
+    std::cout << "LeetCode Problem: " << problemName << std::endl;
+    std::cout << "Date: " << date << std::endl;
+    std::cout << "=============================" << std::endl;
+}
 
-        int main() {
-            // Two Sum - Start
-            displayHeader("2025-03-19", "Two Sum");
+int main()
+{
+    // Two Sum - Start
+    displayHeader("2025-03-19", "Two Sum");
 
-            // Two Sum testing code
-            std::vector<int> nums = {2, 7, 11, 15};
-            const std::vector<int> result = TwoSum::twoSum(nums, 9);
+    // Two Sum testing code
+    std::vector<int> nums = {2, 7, 11, 15};
+    const std::vector<int> result = TwoSum::twoSum(nums, 9);
 
-            // Display result
-            std::cout << "The indices are: " << result[0] << " and " << result[1] << std::endl;
+    // Display result
+    std::cout << "The indices are: " << result[0] << " and " << result[1] << std::endl;
 
-            // Two Sum - End
-            return 0;
-        }
+    // Two Sum - End
+    return 0;
+}
